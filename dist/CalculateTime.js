d3f120475679cccc3a04fdbc84391f3bd3d6c8fa
Collins Mutai <collinsfrontend@gmail.com>
2022-08-05 09:59:41 +0300

    test2

--- dist/CalculateTime.js
+++
@@ -1,8 +0,0 @@
-export class CalculateTime {
-    constructor(start) {
-        this.startDate = start;
-    }
-    getTimeDifference() {
-        return new Date().getDate() - new Date(this.startDate).getDate();
-    }
-}
